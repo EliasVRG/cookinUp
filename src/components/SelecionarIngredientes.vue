b1f83a04d1a8fd7072a42ceb60f147a21b2e30a4
TobyNull <eliasvictor2452@gmail.com>
2023-12-26 17:47:34 -0300

    começando projeto em vue.js

---
+++ src/components/SelecionarIngredientes.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export default{}
+</script>
+
+<template>
+    <section class="selecionar-ingredientes">
+        <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
+
+        <p class="paragrafo-lg instrucoes">
+            Selecione abaixo os ingredientes que você quer usar nesta receita:
+        </p>
+
+        <ul class="categorias">
+
+        </ul>
+
+        <p class="paragrafo dica"></p>
+    </section>
+</template>
